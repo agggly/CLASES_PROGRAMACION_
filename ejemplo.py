431f03c1cab4356feda4b20f0f7f17fb14501d4e
Nicolas Turraca <nico.turraca@gmail.com>
2025-04-24 11:53:58 -0300

    Update ejemplo.py
    BASTA LOCO AUXILIO AAAAAAAAAAAAAA

--- ejemplo.py
+++ ejemplo.py
@@ -31,6 +31,12 @@ else:
     print("Reprobaste")
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #Clase del 24/04 - Programacion, teorico es esto. 
 Estructuras de interaccion. 
 1. Que es una interacion?
@@ -68,11 +74,75 @@ EN FOR, LAS LISTAS INICIAN CON 0, EN WHILE, LA LISTA COMIENZA CON 1.
 4. el for evalua la condicion de finalizacion y mientras esta se cumpla continuaran las repeticiones. 
 
 
+(PILDORAS-INFORMATICAS VIDEITOS DE PYTHON/RECOMENDACION) 
+PROGRAMACIOM I ES PROMOCIONAL.
+
+-WHILE.
+    La sentencia while es util en aquellos casos en donde no se conoce de antemano el numero de veces que una o mas sentencias se tienen que repetir. 
+-DO... WHILE.
+    La sentencia do es usada generalmente en cooperacion con while para garantizar que una o mas instruccionnes se ejecuten al menos una vez. 
+
+SENTENCIAS BREAK - CONTINUE
+    En la condicion switch, la sentencia break es utilizada con el proposito de forzar un salto dentro del bloque switch haciael final del mismo. La misma sentencia se puede usar
+    un conjunto con las distintas operaciones de iteracion. Ademas existe una nueva sentencia que se puede incluir en los ciclos.....
+
+        La sentencia break se usa para forzar un salto hacia el final de un ciclo controlado por for o por while.
+            La construccion del cliclo while para el caso de la setnencia break es diferente, esto para garantizar que el  
+            ciclo no vaya a caer en una iteracion infinita. 
+
+- CONTINUE
+    continue es una sentencia que, cuando se encuentra dentro de un bucle (for o while), hace que la iteración actual se interrumpa y el control pase directamente a la siguiente iteración.
+    En otras palabras, continue salta el resto del código dentro del bucle en la iteración actual y vuelve al principio del bucle para la siguiente iteración. 
+
+
+
+
+La ciudad esta dividida en 2 zonas de recoleccion de basura zona norte y zona sur de acuerdo al nombre y segun el tipo de ubicacion centrico o nocentrico.
+
+        La zona norte esta conformada por los algunos barrios centricos cuyo nombre son:
+            - Barrio Paykin    
+            - Barrio Atlantico Sur
+            - Villa Rio negro.
+Y la zona sur por los algunos barrios no centricos>
+    - Barrios mujeres argentinas
+    - Barrio quebracho
+    - Barrio Santa Ines.
+
+Debemos hacer un programa que ingresando el nombre del barrio y la ubicacion, nos informe en que zona se encuentra y la cantidad de veces que accedio a las distintas zonas. El 
+programa finalizara el ingreso de datos hasta que ingresen "FIN".
+
+(PSEINT !!!!!) 
+
+//Proceso Recoleccion
+	Definir barrio, ubicacion Como Cadena 
+	Definir zona_norte, zona_sur Como Entero 
+	zona_norte <-- 0 
+	zona_sur <-- 0 
+	Escribir "Ingrese el nombre del barrio o escriba FIN: " 
+	Leer barrio
+Mientras barrio =! "0" Hacer
+    Escriibir
+
+	 Si ubicacion = norte Entonces
+		si barrio = Barrio Paykin 
+	 FinSi
+	 
+ FinMientras
+ 
+ 
+FinProceso
+
+Mi primer condicion del mientras es que el FIN no sea igual a 0. 
+
+ 
 
 
 
 
 
+
+
+    
 
 
 
